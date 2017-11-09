38d8817c650df28b797cb7607c2b36a7f44f4b3e
xionglong <xionglong58@email.swu.edu.cn>
2017-11-09 19:00:58 +0800

    finish one part

---
+++ jsDOM/test6/positionMessage.js
@@ -0,0 +1,47 @@
+function addLoadEvent(func)
+{
+    var oldOnLoad=window.onload;
+    if(typeof window.onload!="function")
+        window.onload=func;
+    else
+    {
+        window.onload=fuction()
+        {
+            oldOnLoad();
+            func();
+        }
+    }
+}
+function moveElement(elementId,final_x,final_y,iteral)
+{
+    if(!document.getElementById) return false;
+
+    var element=document.getElementById(elementId);
+    var xpos=parseInt(element.style.left);
+    var ypos=parseInt(element.style.top);
+    if((xpos==final_x)&&(ypos==final_y))
+        return true;
+     if(xpos<final_x)
+        xpos++;
+    if(xpos>final_x)
+        xpos--;   
+    if(ypos<final_y)
+        ypos++;
+    if(ypos>final_y)
+        ypos--;
+    element.style.left=xpos+"px";
+    element.style.top=ypos+"px";
+    var repeat="moveElement('"+elementId+"',"+final_x+","+final_y+","+iteral+")";
+    movement=setTimeout(repeat,iteral);   
+}
+function positionMessage()
+{
+    if(!document.getElementById) return false;
+    if(!document.getElementById('message')) return false;
+    var elem=document.getElementById("message");
+    elem.style.position="absolute";
+    elem.style.left="50px";
+    elem.style.top="100px";
+    moveElement('message',500,500,10);    
+}
+addLoadEvent(positionMessage);
